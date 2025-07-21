2d8aefe9edb297f258892f487b8c3b61f2dbd5b6
Millz <millzyyt08@gmail.com>
2025-07-21 23:27:28 +0100

    gamerule

--- src/main/java/dev/millzy/partialkeepinventory/PartialKeepInventory.java
+++ src/main/java/dev/millzy/partialkeepinventory/PartialKeepInventory.java
@@ -2,23 +2,24 @@ package dev.millzy.partialkeepinventory;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.fabricmc.fabric.api.gamerule.v1.rule.EnumRule;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
+
 public class PartialKeepInventory implements ModInitializer {
 	public static final String MOD_ID = "partialkeepinventory";
+	private static final String RULE_NAME = "inventoryPreservation";
+	private static final GameRules.Category RULE_CATEGORY = GameRules.Category.PLAYER;
+	public static final GameRules.Key<EnumRule<InventoryRule>> RULE = GameRuleRegistry.register(RULE_NAME, RULE_CATEGORY, GameRuleFactory.createEnumRule(InventoryRule.OFF));
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    }
 }
