65c983caf8959b1e28aa9d82fc6d447fb24aab1e
Millz <millzyyt08@gmail.com>
2025-07-22 16:49:30 +0100

    fix settings

--- src/main/java/dev/millzy/partialkeepinventory/PreservationSettingsHandler.java
+++ src/main/java/dev/millzy/partialkeepinventory/PreservationSettingsHandler.java
@@ -1,14 +1,22 @@
 package dev.millzy.partialkeepinventory;
 
 import java.util.EnumSet;
+import java.util.stream.Collectors;
 
 public class PreservationSettingsHandler {
-    private final EnumSet<PreservationSettings> preservationSettings = EnumSet.of(PreservationSettings.NONE);
-    private final int SETTINGS_MAX_ORDINAL = EnumSet.allOf(PreservationSettings.class).stream()
+    private final EnumSet<PreservationSettings> preservationSettings = EnumSet.noneOf(PreservationSettings.class);
+    private static final int SETTINGS_MAX_ORDINAL = EnumSet.allOf(PreservationSettings.class).stream()
             .max(PreservationSettings::compareTo).orElse(PreservationSettings.NONE).ordinal();
+
+    public static final EnumSet<PreservationSettings> ALL_SETTINGS;
 
     public PreservationSettingsHandler() {
         this(0);
+    }
+
+    static {
+        ALL_SETTINGS = EnumSet.allOf(PreservationSettings.class);
+        ALL_SETTINGS.remove(PreservationSettings.NONE);
     }
 
     public PreservationSettingsHandler(int value) {
@@ -38,5 +46,9 @@ public class PreservationSettingsHandler {
 
     public void disableSetting(PreservationSettings setting) {
         preservationSettings.remove(setting);
+    }
+
+    public String[] getValueDisplays() {
+        return preservationSettings.stream().map(PreservationSettings::getDisplay).toArray(String[]::new);
     }
 }
