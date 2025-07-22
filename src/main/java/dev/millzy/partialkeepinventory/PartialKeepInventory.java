4841a6dc93d6bbf1fbeba27a0662d56168900ecf
Millz <millzyyt08@gmail.com>
2025-07-22 16:49:57 +0100

    command and attachment registration

--- src/main/java/dev/millzy/partialkeepinventory/PartialKeepInventory.java
+++ src/main/java/dev/millzy/partialkeepinventory/PartialKeepInventory.java
@@ -1,15 +1,18 @@
 package dev.millzy.partialkeepinventory;
 
+import com.mojang.serialization.Codec;
+import dev.millzy.partialkeepinventory.command.PartialKeepInventoryCommand;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
-import net.fabricmc.fabric.api.gamerule.v1.rule.EnumRule;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Method;
 
 public class PartialKeepInventory implements ModInitializer {
 	public static final String MOD_ID = "partialkeepinventory";
@@ -18,9 +21,13 @@ public class PartialKeepInventory implements ModInitializer {
 	private static final GameRules.Category RULE_CATEGORY = GameRules.Category.PLAYER;
 	public static final GameRules.Key<GameRules.BooleanRule> RULE = GameRuleRegistry.register(RULE_NAME, RULE_CATEGORY, GameRuleFactory.createBooleanRule(false));
 
+	public static final AttachmentType<Integer> PRESERVATION_SETTINGS_ATTACHMENT =
+			AttachmentRegistry.createPersistent(Identifier.of(MOD_ID, "preservation_settings"), Codec.INT);
+
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		CommandRegistrationCallback.EVENT.register(PartialKeepInventoryCommand::register);
     }
 }
