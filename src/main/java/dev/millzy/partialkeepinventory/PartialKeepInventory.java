97d2b76efdc6db84004781e351c5aae8b9016c8d
Millz <millzyyt08@gmail.com>
2025-07-22 10:59:44 +0100

    change gamerule

--- src/main/java/dev/millzy/partialkeepinventory/PartialKeepInventory.java
+++ src/main/java/dev/millzy/partialkeepinventory/PartialKeepInventory.java
@@ -13,9 +13,10 @@ import java.lang.reflect.Method;
 
 public class PartialKeepInventory implements ModInitializer {
 	public static final String MOD_ID = "partialkeepinventory";
-	private static final String RULE_NAME = "inventoryPreservation";
+
+	private static final String RULE_NAME = "partialKeepInventory";
 	private static final GameRules.Category RULE_CATEGORY = GameRules.Category.PLAYER;
-	public static final GameRules.Key<EnumRule<InventoryRule>> RULE = GameRuleRegistry.register(RULE_NAME, RULE_CATEGORY, GameRuleFactory.createEnumRule(InventoryRule.OFF));
+	public static final GameRules.Key<GameRules.BooleanRule> RULE = GameRuleRegistry.register(RULE_NAME, RULE_CATEGORY, GameRuleFactory.createBooleanRule(false));
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
