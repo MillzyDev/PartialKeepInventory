0cd03bae6630b4d9a77bbecb3f908f23079204cb
Millz <millzyyt08@gmail.com>
2025-07-22 16:49:20 +0100

    string rep

--- src/main/java/dev/millzy/partialkeepinventory/PreservationSettings.java
+++ src/main/java/dev/millzy/partialkeepinventory/PreservationSettings.java
@@ -1,15 +1,35 @@
 package dev.millzy.partialkeepinventory;
 
+import java.util.EnumSet;
+import java.util.Objects;
+
 public enum PreservationSettings {
-    NONE,
-    EQUIPMENT,
-    TOOLS,
-    HOTBAR,
-    OFFHAND,
-    EPIC_ITEMS,
-    RARE_ITEMS,
-    UNCOMMON_ITEMS,
-    COMMON_ITEMS,
-    EXPERIENCE,
-    CUSTOM_LIST
+    NONE("none"),
+    EQUIPMENT("equipment"),
+    TOOLS("tools"),
+    HOTBAR("hotbar"),
+    OFFHAND("offhand"),
+    EPIC_ITEMS("epicItems"),
+    RARE_ITEMS("rareItems"),
+    UNCOMMON_ITEMS("uncommonItems"),
+    COMMON_ITEMS("commonItems"),
+    EXPERIENCE("experience"),
+    CUSTOM_LIST("customList");
+
+    private final String display;
+    private static final EnumSet<PreservationSettings> ALL_VALUES = EnumSet.allOf(PreservationSettings.class);
+
+    PreservationSettings(String display) {
+        this.display = display;
+    }
+
+    public String getDisplay() {
+        return this.display;
+    }
+
+    public static PreservationSettings fromString(String str) {
+        return ALL_VALUES.stream()
+                .filter(v -> Objects.equals(v.display, str))
+                .findFirst().orElse(NONE);
+    }
 }
