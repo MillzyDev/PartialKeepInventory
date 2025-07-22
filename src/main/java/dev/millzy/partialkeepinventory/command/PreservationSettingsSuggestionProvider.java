4521b0905a4264018cccbc1de2013d2f4794a209
Millz <millzyyt08@gmail.com>
2025-07-22 16:50:21 +0100

    preservation settings suggestions

---
+++ src/main/java/dev/millzy/partialkeepinventory/command/PreservationSettingsSuggestionProvider.java
@@ -0,0 +1,24 @@
+package dev.millzy.partialkeepinventory.command;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import dev.millzy.partialkeepinventory.PreservationSettings;
+import dev.millzy.partialkeepinventory.PreservationSettingsHandler;
+import net.minecraft.command.CommandSource;
+import net.minecraft.server.command.ServerCommandSource;
+
+import java.util.concurrent.CompletableFuture;
+
+public class PreservationSettingsSuggestionProvider implements SuggestionProvider<ServerCommandSource> {
+    @Override
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) throws CommandSyntaxException {
+        for (PreservationSettings setting : PreservationSettingsHandler.ALL_SETTINGS) {
+            builder.suggest(setting.getDisplay());
+        }
+
+        return builder.buildFuture();
+    }
+}
