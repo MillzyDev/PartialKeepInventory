afc1b736b0ec1247ab6e7a0b2d8e32ea94ce7c74
Millz <millzyyt08@gmail.com>
2025-07-23 19:14:53 +0100

    item list commands

--- src/main/java/dev/millzy/partialkeepinventory/command/PartialKeepInventoryCommand.java
+++ src/main/java/dev/millzy/partialkeepinventory/command/PartialKeepInventoryCommand.java
@@ -4,11 +4,10 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
-import dev.millzy.partialkeepinventory.PartialKeepInventory;
 import dev.millzy.partialkeepinventory.PreservationSettings;
-import dev.millzy.partialkeepinventory.PreservationSettingsHandler;
 import dev.millzy.partialkeepinventory.PreservationSettingsState;
 import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.argument.ItemStackArgument;
 import net.minecraft.command.argument.ItemStackArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
@@ -46,19 +45,19 @@ public class PartialKeepInventoryCommand {
             )
             .then(
                 CommandManager.literal("itemList")
-                .executes(null) // TODO: Show custom list
+                .executes(PartialKeepInventoryCommand::listItems)
                 .then(
                     CommandManager.literal("add")
                     .then(
                         CommandManager.argument("item", ItemStackArgumentType.itemStack(registryAccess))
-                        //.executes(null) // TODO: Add item to list
+                        .executes(PartialKeepInventoryCommand::addItem)
                     )
                 )
                 .then(
                     CommandManager.literal("remove")
                     .then(
                         CommandManager.argument("item", ItemStackArgumentType.itemStack(registryAccess))
-                        //.executes(null) // TODO: Remove item from list
+                        .executes(PartialKeepInventoryCommand::removeItem)
                     )
                 )
             );
@@ -67,15 +66,13 @@ public class PartialKeepInventoryCommand {
     }
 
     private static int listFeatures(CommandContext<ServerCommandSource> context) {
-        ServerWorld world = context.getSource().getWorld();
+        ServerWorld world = context.getSource().getServer().getOverworld();
 
         PreservationSettingsState settingsState = world.getPersistentStateManager().getOrCreate(PreservationSettingsState.ID);
         String[] enabledSettings = settingsState.getEnabledSettingNames();
         Optional<String> combinedList = Arrays.stream(enabledSettings).reduce((st, v) -> st.concat("\n").concat(v));
 
-        Text message;
-
-        message = combinedList
+        Text message = combinedList
                 .map(s -> Text.of("Enabled features:\n" + s))
                 .orElseGet(() -> Text.of("No features are enabled."));
 
@@ -91,7 +88,7 @@ public class PartialKeepInventoryCommand {
             // TODO: exception
         }
 
-        ServerWorld world = context.getSource().getWorld();
+        ServerWorld world = context.getSource().getServer().getOverworld();
 
         PreservationSettingsState settingsState = world.getPersistentStateManager().getOrCreate(PreservationSettingsState.ID);
         settingsState.enableSetting(setting);
@@ -109,13 +106,52 @@ public class PartialKeepInventoryCommand {
             // TODO: exception
         }
 
-        ServerWorld world = context.getSource().getWorld();
+        ServerWorld world = context.getSource().getServer().getOverworld();
 
         PreservationSettingsState settingsState = world.getPersistentStateManager().getOrCreate(PreservationSettingsState.ID);
         settingsState.disableSetting(setting);
         world.getPersistentStateManager().set(PreservationSettingsState.ID, settingsState);
 
         context.getSource().sendMessage(Text.of("Disabled feature: " + feature));
+        return 0;
+    }
+
+    private static int listItems(CommandContext<ServerCommandSource> context) {
+        ServerWorld world = context.getSource().getServer().getOverworld();
+
+        PreservationSettingsState settingsState = world.getPersistentStateManager().getOrCreate(PreservationSettingsState.ID);
+        String[] items = settingsState.getItemsFromList();
+        Optional<String> combinedList = Arrays.stream(items).reduce((st, v) -> st.concat("\n").concat(v));
+
+        Text message = combinedList
+                .map(s -> Text.of("Item list contents:\n" + s))
+                .orElseGet(() -> Text.of("Item list is empty."));
+
+        context.getSource().sendMessage(message);
+        return 0;
+    }
+
+    private static int addItem(CommandContext<ServerCommandSource> context) {
+        ServerWorld world = context.getSource().getServer().getOverworld();
+        ItemStackArgument item = ItemStackArgumentType.getItemStackArgument(context, "item");
+
+        PreservationSettingsState settingsState = world.getPersistentStateManager().getOrCreate(PreservationSettingsState.ID);
+        settingsState.addToItemList(item.getItem().getDefaultStack());
+        world.getPersistentStateManager().set(PreservationSettingsState.ID, settingsState);
+
+        context.getSource().sendMessage(Text.of("Added item: " + item.getItem().toString()));
+        return 0;
+    }
+
+    private static int removeItem(CommandContext<ServerCommandSource> context) {
+        ServerWorld world = context.getSource().getServer().getOverworld();
+        ItemStackArgument item = ItemStackArgumentType.getItemStackArgument(context, "item");
+
+        PreservationSettingsState settingsState = world.getPersistentStateManager().getOrCreate(PreservationSettingsState.ID);
+        settingsState.removeFromItemList(item.getItem().getDefaultStack());
+        world.getPersistentStateManager().set(PreservationSettingsState.ID, settingsState);
+
+        context.getSource().sendMessage(Text.of("Removed item: " + item.getItem().toString()));
         return 0;
     }
 }
