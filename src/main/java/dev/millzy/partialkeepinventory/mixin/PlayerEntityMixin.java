1522e630797490223d1b7e815f5824e93a4fb591
Millz <millzyyt08@gmail.com>
2025-07-23 10:40:38 +0100

    experience feature

--- src/main/java/dev/millzy/partialkeepinventory/mixin/PlayerEntityMixin.java
+++ src/main/java/dev/millzy/partialkeepinventory/mixin/PlayerEntityMixin.java
@@ -2,10 +2,9 @@ package dev.millzy.partialkeepinventory.mixin;
 
 import dev.millzy.partialkeepinventory.InventorySlotChecker;
 import dev.millzy.partialkeepinventory.PartialKeepInventory;
+import dev.millzy.partialkeepinventory.PreservationSettings;
 import dev.millzy.partialkeepinventory.PreservationSettingsHandler;
-import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
@@ -13,11 +12,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,14 +23,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
     @Shadow public int experienceLevel;
-
     @Shadow @Final PlayerInventory inventory;
 
     @Shadow protected abstract void vanishCursedItems();
-
-    @Shadow public abstract PlayerInventory getInventory();
-
-    @Shadow @Nullable public abstract ItemEntity dropItem(ItemStack stack, boolean retainOwnership);
 
     private final int MAX_SLOT = PlayerInventory.MAIN_SIZE + PlayerInventory.EQUIPMENT_SLOTS.size();
 
@@ -43,41 +35,12 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 
     @Inject(at = @At("HEAD"), method = "getExperienceToDrop", cancellable = true)
     private void getExperienceToDropHead(ServerWorld world, CallbackInfoReturnable<Integer> info) {
-        int ret = !this.isSpectator() ? Math.min(this.experienceLevel * 7, 100) : 0;
+        int settingsFlags = world.getAttachedOrCreate(PartialKeepInventory.PRESERVATION_SETTINGS_ATTACHMENT, () -> 0);
+        PreservationSettingsHandler preservationSettings = new PreservationSettingsHandler(settingsFlags);
+
+        boolean preserveExperience = preservationSettings.getSetting(PreservationSettings.EXPERIENCE);
+        int ret = !preserveExperience && !this.isSpectator() ? Math.min(this.experienceLevel * 7, 100) : 0;
         info.setReturnValue(ret);
-    }
-
-    @Unique
-    private void dropInventoryNoHotbar() {
-        // Skip the first 9 slots, as this is the hotbar
-        for (int i = 9; i < this.inventory.getMainStacks().size(); i++) {
-            ItemStack itemStack = this.inventory.getMainStacks().get(i);
-
-            if (itemStack.isEmpty()) {
-                continue;
-            }
-
-            this.dropItem(itemStack, true, false);
-            this.inventory.getMainStacks().set(i, ItemStack.EMPTY);
-        }
-    }
-
-    @Unique
-    private void dropInventoryNoTools() {
-        // We check all slots including up to the offhand
-        for (int i = 0; i < PlayerInventory.OFF_HAND_SLOT + 1; i++) {
-            ItemStack itemStack = this.inventory.getStack(i);
-
-            itemStack.streamTags().forEach(tag -> PartialKeepInventory.LOGGER.info(tag.id().toString()));
-
-            if (itemStack.getComponents().stream().anyMatch(
-                    c -> c.type() == DataComponentTypes.TOOL || c.type() == DataComponentTypes.EQUIPPABLE)) {
-                continue;
-            }
-
-            this.dropItem(itemStack, true, false);
-            this.inventory.setStack(i, ItemStack.EMPTY);
-        }
     }
 
     @Inject(at = @At("HEAD"), method = "dropInventory", cancellable = true)
@@ -105,5 +68,7 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             this.dropItem(itemStack, true, false);
             this.inventory.setStack(i, ItemStack.EMPTY);
         }
+
+        this.vanishCursedItems();
     }
 }
